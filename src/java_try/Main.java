954a71db9750e1a483b6fca4cf6183f72414cd74
coma-dwc <tamagomapon@gmail.com>
2023-08-24 21:53:41 +0900

    printStackTrace

--- src/java_try/Main.java
+++ src/java_try/Main.java
@@ -1,10 +1,34 @@
 package java_try;
 
+//public class Main {
+//	private static int divide(int a, int b) {
+//		try {
+//			return a / b;
+//		} catch (ArithmeticException e) {
+//		  return 0;
+//		}
+//	}
+//	
+//	public static void main(String[] args) {
+//		System.out.println("テスト1: " + divide(12, 4));
+//		System.out.println("テスト2: " + divide(12, 0));
+//		System.out.println("テスト3: " + divide(12, 2));
+//	}
+//}
+//try ～ catchを使用することでtryのブロックで発生したエラーを捕捉して、
+//エラー処理を行うことができる
+//ここではArithmeticExceptionを捕捉して、それが発生した場合は0を返している
+
+
+//printStackTrace
+//例外捕捉時に発生したExceptionのprintStackTrace()メソッドを実行
+
 public class Main {
 	private static int divide(int a, int b) {
 		try {
 			return a / b;
 		} catch (ArithmeticException e) {
+		  e.printStackTrace();
 		  return 0;
 		}
 	}
@@ -15,6 +39,4 @@ public class Main {
 		System.out.println("テスト3: " + divide(12, 2));
 	}
 }
-//try ～ catchを使用することでtryのブロックで発生したエラーを捕捉して、
-//エラー処理を行うことができる
-//ここではArithmeticExceptionを捕捉して、それが発生した場合は0を返している
+//printStackTrace()は実行することで発生したExceptionのスタックトレースを出力することが出来る
