a19816c6a639cc03a547332c8b6bfa6e3f198712
coma-dwc <tamagomapon@gmail.com>
2023-08-25 22:22:49 +0900

    ValidationExceptionのサブクラスの作成

--- src/Own_exception/ValidationException.java
+++ src/Own_exception/ValidationException.java
@@ -4,17 +4,53 @@ package Own_exception;
 //・ValidationExceptionの作成
 //バリデーションエラーを示す例外、ValidationExceptionをExceptionを継承して定義する
 
-public class ValidationException extends Exception {
-	public ValidationException(String message) {
-		super(message);
-	}
-	
-	public ValidationException(String message, Throwable couse) {
-		super(message, couse);
-	}
-}
+//public class ValidationException extends Exception {
+//	public ValidationException(String message) {
+//		super(message);
+//	}
+//	
+//	public ValidationException(String message, Throwable couse) {
+//		super(message, couse);
+//	}
+//}
 
 //ここではExceptionを継承して作成しているのでValidationExceptionは検知例外となる
 //非検知例外としたい場合はRuntimeExceptionを継承して作成
 //Exceptionを作成する場合はコンストラクタでは、エラーメッセージを引数とするものの他に
 //原因となるExceptionを示す、causeを引数とするものを作っておくと汎用性が上がる
+
+
+//・ValidationExceptionのサブクラスの作成
+//ValidationExceptionを継承したUsernameExceptionとPasswordExceptionを作成
+
+public class ValidationException extends Exception {
+	public ValidationException(String message) {
+		super(message);
+	}
+	
+	public ValidationException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
+
+class UsernameException extends ValidationException {
+	
+	public UsernameException(String message) {
+		super(message);
+	}
+	
+	public UsernameException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
+
+class PasswordException extends ValidationException {
+	
+	public PasswordException(String message) {
+		super(message);
+	}
+	
+	public PasswordException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
