ac05879f2d76b6ed1273e33a3ccaecfae5b76320
coma-dwc <tamagomapon@gmail.com>
2023-08-25 21:21:31 +0900

    NullPointerException

--- src/RuntimeException/Main.java
+++ src/RuntimeException/Main.java
@@ -3,17 +3,17 @@ package RuntimeException;
 //・割り算のメソッド(再)
 //divide()のサンプルを改変してtry～catchの部分を取り除く
 
-public class Main {
-	private static int divide(int a, int b) {
-		return a / b;
-	}
-	
-	public static void main(String[] args) {
-		System.out.println("テスト1: " + divide(12, 4));
-		System.out.println("テスト2: " + divide(12, 0));
-		System.out.println("テスト3: " + divide(12, 2));
-	}
-}
+//public class Main {
+//	private static int divide(int a, int b) {
+//		return a / b;
+//	}
+//	
+//	public static void main(String[] args) {
+//		System.out.println("テスト1: " + divide(12, 4));
+//		System.out.println("テスト2: " + divide(12, 0));
+//		System.out.println("テスト3: " + divide(12, 2));
+//	}
+//}
 
 //divide()メソッドでは0除算が発生した場合にArithmeticExceptionが発生する
 //しかし、try～catchで括られていないにも関わらずコンパイルエラーとはならず、
@@ -23,3 +23,14 @@ public class Main {
 //クラス構造的には非検査例外はRuntimeExceptionのサブクラス
 //※非検査例外がなぜcatchを強制されないかというと、非検査例外の発生する原因が
 //ほとんどの場合バグだから
+
+//・NullPointerException
+//変数strにnullを代入してNullPointerExceptionを発生させる
+public class Main {
+	public static void main(String[] args) {
+		String str = null;
+		System.out.println(str.toUpperCase());
+	}
+}
+//この例では変数strはString型なので、String型の持つメソッドtoUpperCase()を実行可能なはずですが、
+//値がnullだとメソッドの実行が出来ないためNullPointerExceptionとなる
